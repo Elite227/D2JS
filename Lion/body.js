9f282ec53ac1f5c4231c655e350dadcd2e426c5f
Elite227 <koad2282@mail.ru>
2016-07-17 19:31:48 +0300

    Update body.js

--- Lion/body.js
+++ Lion/body.js
@@ -1,4 +1,4 @@
-﻿/*-----------------------------------------------
+/*-----------------------------------------------
 //////////////// vk.com/d2jscripts //////////////
 /////////////////////////////////////////////////
 Авто Ульт на Лиона.
@@ -11,6 +11,46 @@ var damage = [490,600,720]
 var scepterdamage = [570,710,890]
 var manacost = [200,420,625]
 var rangeCast = 900
+var debug = false
+
+var IgnoreBuffs = [
+	"modifier_abaddon_borrowed_time",
+	"modifier_brewmaster_primal_split",
+	"modifier_omniknight_repel",
+	"modifier_phoenix_supernova_hiding",
+	"modifier_tusk_snowball_movement",
+	"modifier_tusk_snowball_movement_friendly",
+	"modifier_juggernaut_blade_fury",
+	"modifier_medusa_stone_gaze",
+	"modifier_nyx_assassin_spiked_carapace",
+	"modifier_templar_assassin_refraction_absorb",
+	"modifier_oracle_false_promise",
+	"modifier_dazzle_shallow_grave",
+	"modifier_treant_living_armor",
+	"modifier_life_stealer_rage",
+	"modifier_item_aegis"
+]
+
+var DebuffsAddMagicDmg = [
+	["modifier_bloodthorn_debuff", 1.3],
+	["modifier_orchid_malevolence_debuff", 1.3],
+	["modifier_item_mask_of_madness_berserk", 1.25],
+	["modifier_bloodseeker_bloodrage", [1.25,1.3,1.35,1.4]],
+	["modifier_ursa_enrage", 0.2],
+]
+
+var BuffsAbsorbMagicDmg = [
+	["modifier_item_pipe_barrier", 400],
+	["modifier_item_hood_of_defiance_barrier", 400],
+	["modifier_item_infused_raindrop", 120],
+	["modifier_abaddon_aphotic_shield", [110,140,170,200]],
+	["modifier_ember_spirit_flame_guard", [50,200,350,500]]
+]
+
+var BuffsAddMagicDmgForMe = [
+	["item_aether_lens", 1.05],
+	["modifier_bloodseeker_bloodrage", [1.25,1.3,1.35,1.4]]
+
 function LionF(){
 if ( !Lion.checked )
 		return
